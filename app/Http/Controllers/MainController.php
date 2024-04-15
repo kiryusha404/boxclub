6e52a5218292a9f72df6437ff60c9980db5634f0
Kirill <akvamarin0610@gmail.com>
2024-04-15 22:14:32 +0400

    fix bag feedback

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -46,7 +46,12 @@ class MainController extends Controller
 
     //страница отзывов
     public function feedback(){
-        $id = Auth()->User()->id;
+        if(Auth()->User()) {
+            $id = Auth()->User()->id;
+        }
+        else{
+            $id = 0;
+        }
         $form = DB::table('feedback')->where('feedback.user_id', '=', $id)->count('feedback.id');
         $feedbacks = DB::table('feedback')->join('users', 'users.id', '=', 'feedback.user_id')->orderBy('feedback.date', 'desc')->get();
         return view('feedback', ['form' => $form, 'feedbacks' => $feedbacks]);
